c5e7c35ea0633bacc67f2c1ec8a2d9b71ef6f5c7
Kaushikprahaladh <kaushikprahaladh403@gmail.com>
2022-07-12 23:04:22 +0530

    init

---
+++ Module4 solution/SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function(window){
+    var speakgoodbye={
+        function(name){
+            console.log("GoodBye "+ name);
+        }
+    };
+    window.speakgoodbye=speakgoodbye;
+})(window);
---
+++ Module4 solution/SpeakHello.js
@@ -0,0 +1,8 @@
+(function (window){
+    var speakhello= {
+        funtion(name){
+            console.log("Hello "+ name);
+        }
+    };
+    window.speakhello=speakhello;
+})(window);
