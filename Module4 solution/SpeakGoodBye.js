c6045fdc61caef33573319ba1258214b8016163e
Kaushikprahaladh <kaushikprahaladh403@gmail.com>
2022-07-12 23:36:39 +0530

    init

--- Module4 solution/SpeakGoodBye.js
+++ Module4 solution/SpeakGoodBye.js
@@ -1,7 +1,7 @@
 (function(window){
     var speakword="GoodBye ";
-    var speakgoodbye=function(name){
-        console.log(speakword +name );
+    var speakgoodbye=(name) => {
+        console.log(speakword + name);
     }
     window.speakgoodbye=speakgoodbye;
 })(window);
